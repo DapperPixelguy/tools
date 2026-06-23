41c3e90d3b64fd962b348e58a6ea60f597cadcde
DapperPixelguy <32307494+DapperPixelguy@users.noreply.github.com>
2026-06-23 20:23:23 +0100

    get_tools function to parse tools directory into clickable buttons

--- website/main.py
+++ website/main.py
@@ -1,8 +1,34 @@
+import os, yaml
+
 from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+def get_tools():
+    TOOLS_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'tools')
+    tools_list = []
+
+    class Tool():
+        name: str
+        content: str
+
+    for tool in os.listdir(TOOLS_DIRECTORY):
+        new_tool = Tool()
+        TOOL_DIRECTORY = os.path.join(TOOLS_DIRECTORY, tool)
+        CONFIG_FILE = os.path.join(TOOL_DIRECTORY, 'config.yaml')
+
+        with open(CONFIG_FILE, 'r') as f:
+            data = yaml.safe_load(f)
+
+            name = data.get('tool_name')
+            TOOL_CONTENT = os.path.join(TOOL_DIRECTORY, data.get('content'))
+
+            new_tool.name = name
+            new_tool.content = TOOL_CONTENT
+        tools_list.append(new_tool)
+    return tools_list
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tools=get_tools())
 
