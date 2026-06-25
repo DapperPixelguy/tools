574bf7d2ce0d5e75be0c2231f68b40b00645f31f
DapperPixelguy <32307494+DapperPixelguy@users.noreply.github.com>
2026-06-25 17:33:06 +0100

    Load tools from tools directory and build based on config file

--- website/main.py
+++ website/main.py
@@ -1,21 +1,25 @@
 import os, yaml
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 main = Blueprint('main', __name__)
 
+TOOLS_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'tools')
+
 def get_tools():
-    TOOLS_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'tools')
     tools_list = []
+    print(TOOLS_DIRECTORY)
 
     class Tool():
         name: str
+        hash: str
         content: str
 
     for tool in os.listdir(TOOLS_DIRECTORY):
         new_tool = Tool()
         TOOL_DIRECTORY = os.path.join(TOOLS_DIRECTORY, tool)
         CONFIG_FILE = os.path.join(TOOL_DIRECTORY, 'config.yaml')
+        print(CONFIG_FILE)
 
         with open(CONFIG_FILE, 'r') as f:
             data = yaml.safe_load(f)
@@ -29,6 +33,11 @@ def get_tools():
             new_tool.content = TOOL_CONTENT
         tools_list.append(new_tool)
     return tools_list
+
+@main.route('/tools/<tool_name>/<path:filename>')
+def tool_files(tool_name, filename):
+    print(os.path.join(TOOLS_DIRECTORY, tool_name, filename))
+    return send_from_directory(os.path.join(TOOLS_DIRECTORY, tool_name), filename)
 
 @main.route('/')
 def index():
